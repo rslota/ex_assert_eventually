09b8da6bbc690169f90f11302c4dec105eb637d4
Rafal Slota <rafal.slota92@gmail.com>
2022-02-09 20:20:10 +0100

    fixup! Don't use variables within loop fun

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAssertEventually.MixProject do
   def project do
     [
       app: :assert_eventually,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
